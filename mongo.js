7d0d4b64de87e9c8e82bb67ecb5eecb006ab1cb8
Guillermo Thourey <TrollScien@users.noreply.github.com>
2022-03-23 17:00:40 -0400

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const connectionString = 'mongodb+srv://guillermo:OtC5FEYz9qSDYwUC@cluster0.qxuyg.mongodb.net/notes?retryWrites=true&w=majority'
+const connectionString = ''
 const {Schema} = mongoose
 //conexion a mongodb
 
